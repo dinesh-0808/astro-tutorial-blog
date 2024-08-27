a6cd37463edb5ee676b6c6c777037d9e7266693d
dinesh <dineshc@aissel.com>
2024-08-27 10:18:14 +0530

    fifth commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com"
+  site: "http://localhost:4321",
+  integrations: [preact()]
 });
